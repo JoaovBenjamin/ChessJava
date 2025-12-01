e05aa533d3f973806a3f606bb0901a35046bfa06
Joaovvb <rm98938@fiap.com.br>
2025-12-01 20:54:36 -0300

    Cirando classes Piece e Board do meu game

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import boardgame.Board;
 import boardgame.Position;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    Position pos = new Position(3,4);
+    Board boardgame = new Board(8,8);
 
-    IO.println(pos.toString());
+    IO.println(boardgame.getColums());
+
+
 }
